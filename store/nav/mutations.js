44f4d0750469293ce0529c825fc4d61ae686c8f5
Tobias Doerner <tobias.doerner@mailbox.org>
2020-12-28 20:54:54 +0100

    Removed unused components

--- store/nav/mutations.js
+++
@@ -1,9 +0,0 @@
-export default {
-  SET_CLIPPED(state, payload) {
-    state.clipped = payload
-  },
-
-  SET_DRAWER(state, payload) {
-    state.drawer = payload
-  }
-}
--- store/nav/state.js
+++
@@ -1,4 +0,0 @@
-export default () => ({
-  clipped: false,
-  drawer: false
-})
