2a3dbc14f461b7e02a3283092fbec303c9d9e360
Tobias Doerner <tobias.doerner@mailbox.org>
2024-02-11 13:24:04 +0100

    Activate compression of public assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/eslint-module', '@nuxtjs/i18n'],
+  nitro: {
+    compressPublicAssets: true,
+  },
   ssr: false,
   i18n: {
     vueI18n: './i18n.config.ts',
